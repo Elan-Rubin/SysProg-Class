6fe10a520dd2ddeefe980b51417130614776705d
elan_rubin <elandanielrubin@gmail.com>
2025-01-19 18:58:47 -0500

    added replacement option

--- assignments/1-C-Refresher/starter/stringfun.c
+++ assignments/1-C-Refresher/starter/stringfun.c
@@ -15,7 +15,10 @@ int count_words(char *, int, int);
 int print_words(char *, int, int);
 int reverse_str(char *, int, int);
 
-int setup_buff(char *buff, char *user_str, int len) {
+int replace_str(char *buff, int len, int str_len, char *find, char *replace);
+
+int setup_buff(char *buff, char *user_str, int len)
+{
     // TODO: #4:  Implement the setup buff as per the directions
     char *src = user_str;
     char *dst = buff;
@@ -23,19 +26,25 @@ int setup_buff(char *buff, char *user_str, int len) {
     int real_len = 0;
     int check_space = 0;
 
-    while (*src != '\0') {
-        if (count >= len) {
+    while (*src != '\0')
+    {
+        if (count >= len)
+        {
             return -1;
         }
-        if (*src == ' ' || *src == '\t') {
-            if (!check_space && count > 0) {
+        if (*src == ' ' || *src == '\t')
+        {
+            if (!check_space && count > 0)
+            {
                 *dst = ' ';
                 dst++;
                 count++;
                 real_len++;
                 check_space = 1;
             }
-        } else {
+        }
+        else
+        {
             *dst = *src;
             dst++;
             count++;
@@ -45,7 +54,8 @@ int setup_buff(char *buff, char *user_str, int len) {
         src++;
     }
 
-    while (count < len) {
+    while (count < len)
+    {
         *dst = '.';
         dst++;
         count++;
@@ -115,35 +125,143 @@ int reverse_str(char *buff, int len, int str_len)
     return 0;
 }
 
-int print_words(char *buff, int len, int str_len) {
+int print_words(char *buff, int len, int str_len)
+{
     char *start = buff;
     char *current = buff;
     int word_count = 0;
     int char_count = 0;
     printf("Word Print\n----------\n");
 
-    while (current <= buff + str_len) {
-        if (current == buff + str_len || *current == ' ') {
-            if (char_count > 0) {
+    while (current <= buff + str_len)
+    {
+        if (current == buff + str_len || *current == ' ')
+        {
+            if (char_count > 0)
+            {
                 word_count++;
                 printf("%d. ", word_count);
-                
+
                 char *word_ptr = start;
-                while (word_ptr < current) {
+                while (word_ptr < current)
+                {
                     putchar(*word_ptr);
                     word_ptr++;
                 }
-                
+
                 printf(" (%d)\n", char_count);
                 char_count = 0;
                 start = current + 1;
             }
-        } else {
+        }
+        else
+        {
             char_count++;
         }
         current++;
     }
     return 0;
+}
+
+int replace_str(char *buff, int len, int str_len, char *find, char *replace)
+{
+    int find_len = 0;
+    int replace_len = 0;
+    char *temp = find;
+    while (*temp != '\0')
+    {
+        find_len++;
+        temp++;
+    }
+    temp = replace;
+    while (*temp != '\0')
+    {
+        replace_len++;
+        temp++;
+    }
+
+    char *start = buff;
+    char *end = buff + str_len;
+    char *match_start = NULL;
+
+    while (start <= end - find_len)
+    {
+        int matched = 1;
+        for (int i = 0; i < find_len; i++)
+        {
+            if (*(start + i) != *(find + i))
+            {
+                matched = 0;
+                break;
+            }
+        }
+
+        if (matched)
+        {
+            match_start = start;
+            break;
+        }
+        start++;
+    }
+
+    if (match_start == NULL)
+    {
+        printf("'%s' not found\n", find);
+        return -1;
+    }
+
+    int new_len = str_len - find_len + replace_len;
+    if (new_len > len)
+    {
+        printf("Error: buffer size\n");
+        return -1;
+    }
+
+    if (find_len != replace_len)
+    {
+        char *src = match_start + find_len;
+        char *dst = match_start + replace_len;
+
+        if (replace_len < find_len)
+        {
+            while (src < buff + str_len)
+            {
+                *dst = *src;
+                src++;
+                dst++;
+            }
+            while (dst < buff + len)
+            {
+                *dst = '.';
+                dst++;
+            }
+        }
+        else
+        {
+            src = buff + str_len - 1;
+            dst = src + (replace_len - find_len);
+            while (src >= match_start + find_len)
+            {
+                *dst = *src;
+                src--;
+                dst--;
+            }
+        }
+    }
+
+    for (int i = 0; i < replace_len; i++)
+    {
+        *(match_start + i) = *(replace + i);
+    }
+
+    printf("Modified String: ");
+    for (int i = 0; i < new_len; i++)
+    {
+        putchar(*(buff + i));
+    }
+    printf("\n");
+
+    return new_len;
 }
 
 // ADD OTHER HELPER FUNCTIONS HERE FOR OTHER REQUIRED PROGRAM OPTIONS
@@ -239,6 +357,21 @@ int main(int argc, char *argv[])
             printf("Error printing, rc = %d", rc);
             exit(2);
         }
+        break;
+    case 'x':
+        if (argc < 5)
+        {
+            printf("Error: insufficient arguments\n");
+            usage(argv[0]);
+            exit(2);
+        }
+        rc = replace_str(buff, BUFFER_SZ, user_str_len, argv[3], argv[4]);
+        if (rc < 0)
+        {
+            printf("Replacement error, rc = %d\n", rc);
+            exit(2);
+        }
+        user_str_len = rc;
         break;
     default:
         usage(argv[0]);
