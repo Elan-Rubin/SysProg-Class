371d1e8b9a815f1ac4f759402b538c08aa448314
elan_rubin <elandanielrubin@gmail.com>
2025-03-03 18:53:23 -0500

    comp

--- assignments/5-ShellP3/questions.md
+++ assignments/5-ShellP3/questions.md
@@ -1,15 +1,15 @@
 1. Your shell forks multiple child processes when executing piped commands. How does your implementation ensure that all child processes complete before the shell continues accepting user input? What would happen if you forgot to call waitpid() on all child processes?
 
-_answer here_
+We use waitpid() in a loop to wait for each of the child process to finish befre continuing. If this wasn't part of the code, then it would be possible for the child processes to become zombies/detached, and then they'd just waste resources. This could make it unpredictable and slow.
 
 2. The dup2() function is used to redirect input and output file descriptors. Explain why it is necessary to close unused pipe ends after calling dup2(). What could go wrong if you leave pipes open?
 
-_answer here_
+If you don't close the unused ends of a pipe after using dup2(), you might run into some annoying issues. For one, you could end up with file descriptor leaks, which are basically like leaving a bunch of doors open in your house. Also, if you leave the write end of a pipe open, the reading process might never get the signal that there's no more data coming (EOF), and it could just sit there waiting forever. Plus, each process can only handle so many file descriptors, so if you don't close them, you'll run out pretty quickly.
 
 3. Your shell recognizes built-in commands (cd, exit, dragon). Unlike external commands, built-in commands do not require execvp(). Why is cd implemented as a built-in rather than an external command? What challenges would arise if cd were implemented as an external process?
 
-_answer here_
+The cd command must be built-in because it needs to change the shell process's own working directory. If implemented as an external command, it would change the directory in its child process only, leaving the shell's directory unchanged. This process isolation makes it impossible for external commands to modify the parent shell's state.
 
 4. Currently, your shell supports a fixed number of piped commands (CMD_MAX). How would you modify your implementation to allow an arbitrary number of piped commands while still handling memory allocation efficiently? What trade-offs would you need to consider?
 
-_answer here_
+I'd switch to using dynamic arrays that can grow using realloc instead of fixed-size arrays. This comes with some downsides though - the code gets more complex, there's some performance hit from memory management, and you have to handle more error cases. Probably best to start with a decent-sized array and just double it whenever you need more space.
--- assignments/5-ShellP3/starter/dshlib.c
+++ assignments/5-ShellP3/starter/dshlib.c
@@ -6,54 +6,222 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
-
 #include "dshlib.h"
 
-/*
- * Implement your exec_local_cmd_loop function by building a loop that prompts the 
- * user for input.  Use the SH_PROMPT constant from dshlib.h and then
- * use fgets to accept user input.
- * 
- *      while(1){
- *        printf("%s", SH_PROMPT);
- *        if (fgets(cmd_buff, ARG_MAX, stdin) == NULL){
- *           printf("\n");
- *           break;
- *        }
- *        //remove the trailing \n from cmd_buff
- *        cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
- * 
- *        //IMPLEMENT THE REST OF THE REQUIREMENTS
- *      }
- * 
- *   Also, use the constants in the dshlib.h in this code.  
- *      SH_CMD_MAX              maximum buffer size for user input
- *      EXIT_CMD                constant that terminates the dsh program
- *      SH_PROMPT               the shell prompt
- *      OK                      the command was parsed properly
- *      WARN_NO_CMDS            the user command was empty
- *      ERR_TOO_MANY_COMMANDS   too many pipes used
- *      ERR_MEMORY              dynamic memory management failure
- * 
- *   errors returned
- *      OK                     No error
- *      ERR_MEMORY             Dynamic memory management failure
- *      WARN_NO_CMDS           No commands parsed
- *      ERR_TOO_MANY_COMMANDS  too many pipes used
- *   
- *   console messages
- *      CMD_WARN_NO_CMD        print on WARN_NO_CMDS
- *      CMD_ERR_PIPE_LIMIT     print on ERR_TOO_MANY_COMMANDS
- *      CMD_ERR_EXECUTE        print on execution failure of external command
- * 
- *  Standard Library Functions You Might Want To Consider Using (assignment 1+)
- *      malloc(), free(), strlen(), fgets(), strcspn(), printf()
- * 
- *  Standard Library Functions You Might Want To Consider Using (assignment 2+)
- *      fork(), execvp(), exit(), chdir()
- */
-int exec_local_cmd_loop()
-{
-   
+int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff) {
+    char *p = NULL;
+    int i;
+    int start_token = 0;
+    int start_quote = 0;
+
+    for (i = 0, p = cmd_line; *p != '\0'; p++) {
+        if (*p == '"') {
+            if (start_quote) {
+                start_quote = 0;
+                start_token = 0;
+            } else {
+                start_quote = 1;
+            }
+            *p = '\0';
+        } else if (*p == SPACE_CHAR && !start_quote) {
+            start_token = 0;
+            *p = '\0';
+        } else if (*p == PIPE_CHAR && !start_quote) {
+            *p = '\0';
+            p++;
+            break;
+        } else if (!start_token) {
+            start_token = 1;
+            cmd_buff->argv[i] = p;
+            i++;
+        }
+    }
+    cmd_buff->argc = i;
+    cmd_buff->_cmd_buffer = cmd_line;
+
+    return p - cmd_line;
+}
+
+int build_cmd_list(char *cmd_line, command_list_t *clist) {
+    cmd_buff_t *cmd_buf = clist->commands;
+    int rc = OK;
+    int i = 0;
+    int num_cmds = 0;
+
+    while (*cmd_line != '\0' && rc == OK) {
+        if (num_cmds == CMD_MAX) {
+            rc = ERR_TOO_MANY_COMMANDS;
+            break;
+        }
+
+        i = build_cmd_buff(cmd_line, cmd_buf);
+        cmd_buf++;
+        num_cmds++;
+        cmd_line += i;
+    }
+
+    if (num_cmds == 0) {
+        rc = WARN_NO_CMDS;
+    }
+
+    clist->num = num_cmds;
+
+    return rc;
+}
+
+int exec_cmd(cmd_buff_t *cmd) {
+    int child_pid = fork();
+    
+    if (child_pid < 0) {
+        printf("Error: Could not create process\n");
+        return ERR_MEMORY;
+    }
+    
+    if (child_pid == 0) {
+        execvp(cmd->argv[0], cmd->argv);
+        printf("Error: Command not found: %s\n", cmd->argv[0]);
+        exit(ERR_EXEC_CMD);
+    }
+    
+    int status;
+    waitpid(child_pid, &status, 0);
+    return WEXITSTATUS(status);
+}
+
+Built_In_Cmds match_command(const char *input) {
+    if (strcmp(input, EXIT_CMD) == 0) {
+        return BI_CMD_EXIT;
+    } else if (strcmp(input, "cd") == 0) {
+        return BI_CMD_CD;
+    } else if (strcmp(input, "dragon") == 0) {
+        return BI_CMD_DRAGON;
+    } else {
+        return BI_NOT_BI;
+    }
+}
+
+Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd) {
+    Built_In_Cmds bi_cmd;
+    bi_cmd = match_command(cmd->argv[0]);
+    int rc;
+
+    switch (bi_cmd) {
+    case BI_CMD_EXIT:
+        printf("exiting...\n");
+        return BI_CMD_EXIT;
+    case BI_CMD_CD:
+        rc = chdir(cmd->argv[1]);
+        if (rc != 0) {
+            return BI_CMD_CD;
+        }
+        return BI_EXECUTED;
+    case BI_NOT_BI:
+        return BI_NOT_BI;
+    case BI_CMD_DRAGON:
+        return BI_CMD_DRAGON;
+    default:
+        fprintf(stderr, "Fatal error");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int execute_pipeline(command_list_t *clist) {
+    int pipes[clist->num - 1][2];
+    pid_t pids[clist->num];
+
+    for (int i = 0; i < clist->num - 1; i++) {
+        if (pipe(pipes[i]) == -1) {
+            perror("pipe");
+            return ERR_EXEC_CMD;
+        }
+    }
+
+    for (int i = 0; i < clist->num; i++) {
+        pids[i] = fork();
+        if (pids[i] == -1) {
+            perror("fork");
+            return ERR_EXEC_CMD;
+        }
+
+        if (pids[i] == 0) {
+            if (i > 0) {
+                dup2(pipes[i - 1][0], STDIN_FILENO);
+            }
+
+            if (i < clist->num - 1) {
+                dup2(pipes[i][1], STDOUT_FILENO);
+            }
+
+            for (int j = 0; j < clist->num - 1; j++) {
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
+
+            execvp(clist->commands[i].argv[0], clist->commands[i].argv);
+            perror("execvp");
+            exit(ERR_EXEC_CMD);
+        }
+    }
+
+    for (int i = 0; i < clist->num - 1; i++) {
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+    }
+
+    for (int i = 0; i < clist->num; i++) {
+        waitpid(pids[i], NULL, 0);
+    }
+
+    return OK;
+}
+
+int exec_local_cmd_loop() {
+    char *cmd_input;
+    int rc = 0;
+    command_list_t cmd_list = {0};
+    cmd_buff_t *cmd_buff = cmd_list.commands;
+    Built_In_Cmds bi_cmd;
+
+    cmd_input = malloc(SH_CMD_MAX);
+    if (cmd_input == NULL) {
+        perror("malloc:");
+        return ERR_MEMORY;
+    }
+    memset(cmd_input, 0, SH_CMD_MAX);
+
+    while (1) {
+        printf("%s", SH_PROMPT);
+        if (fgets(cmd_input, ARG_MAX, stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+        cmd_input[strcspn(cmd_input, "\n")] = '\0';
+
+        rc = build_cmd_list(cmd_input, &cmd_list);
+        if (rc == WARN_NO_CMDS) {
+            printf(CMD_WARN_NO_CMD);
+            continue;
+        } else if (rc == ERR_TOO_MANY_COMMANDS) {
+            printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
+            continue;
+        }
+
+        if (cmd_list.num == 1) {
+            bi_cmd = exec_built_in_cmd(cmd_buff);
+            if (bi_cmd == BI_CMD_EXIT) {
+                free(cmd_input);
+                return OK;
+            } else if (bi_cmd == BI_NOT_BI) {
+                rc = exec_cmd(cmd_buff);
+                if (rc == ERR_EXEC_CMD) {
+                    printf("Error executing command %s\n", cmd_buff->argv[0]);
+                }
+            }
+        } else {
+            rc = execute_pipeline(&cmd_list);
+        }
+    }
+
+    free(cmd_input);
     return OK;
 }
